74bff494694e753977eda12c1a64f0f6923df703
Ko10PEACE <coltonpierce17@gmail.com>
2023-02-06 15:47:25 -0700

    Update app.js

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
 
+const app = express();
+
+app.user(bodyParser.urlencoded({extended: true}));
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
